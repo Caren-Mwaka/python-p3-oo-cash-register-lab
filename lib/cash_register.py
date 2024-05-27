52f726e4a39a1480699043b51eb44807ee66bc2a
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-05-27 14:37:19 +0300

    modified cash_register.py

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -7,20 +7,32 @@ class CashRegister:
         self.items = []
 
     def add_item(self, title, price, quantity=1):
-        self.total += price * quantity  
+        self.total += price * quantity
         for _ in range(quantity):
             self.items.append(title)
 
+        self.last_transaction = {
+            'title': title,
+            'price': price,
+            'quantity': quantity
+        }
+
     def apply_discount(self):
-        if self.discount > 0:
-            self.total -= self.total * (self.discount / 100)
-            print(f"After the discount, the total comes to ${self.total:.2f}.")  
+        self.total -= self.total * (self.discount / 100)
+        if self.discount > 0:  
+            print(f'After the discount, the total comes to ${int(self.total)}.')
         else:
             print("There is no discount to apply.")
 
     def void_last_transaction(self):
         if self.items:
-            last_item_price = float(self.items.pop())  
+            last_item_price = self.last_transaction['price'] * self.last_transaction['quantity']
             self.total -= last_item_price
+            for _ in range(self.last_transaction['quantity']):
+                self.items.pop()
         else:
-            self.total = 0.0  
+            self.total = 0.0
+
+  
+
+  
